b815dcd9d008d3635e6b91f6394860ff38d096ec
Foram Patel <fragrance9884@gmail.com>
2022-07-19 20:56:38 +0100

    my first commit

---
+++ Datatype.java
@@ -0,0 +1,83 @@
+package Arithmetic;
+
+public class Datatype {
+    public static void main(String[] args) {
+
+
+        int A = 20;
+        int a = 30;
+        System.out.println("Integer Datatye");
+        System.out.println("A+a="+(A+a));
+        System.out.println("A-a="+(A-a));
+        System.out.println("A*a="+(A*a));
+        System.out.println("A/a="+(A/a));
+        System.out.println("A%a="+(A%a));
+
+        short B=22500;
+        short b=32000;
+        System.out.println("Short Datatype");
+        System.out.println("B+b="+(B+b));
+        System.out.println("B-b="+(B-b));
+        System.out.println("B*b="+(B*b));
+        System.out.println("B/b="+(B/b));
+        System.out.println("B%b="+(B%b));
+
+        long C=1245789;
+        long c=1020349;
+
+        System.out.println("Long Datatype");
+        System.out.println("C+c="+(C+c));
+        System.out.println("C-c="+(C-c));
+        System.out.println("C*c="+(C*c));
+        System.out.println("C/c="+(C/c));
+        System.out.println("C%c="+(C%c));
+
+        Byte D=100;
+        Byte d=125;
+
+        System.out.println("Byte Datatype");
+        System.out.println("D+d="+(D+d));
+        System.out.println("D-d="+(D-d));
+        System.out.println("D*d="+(D*d));
+        System.out.println("D/d="+(D/d));
+        System.out.println("D%d="+(D%d));
+
+        Double E=203.13;
+        Double e=185.58;
+
+        System.out.println("Double Datatype");
+        System.out.println("E+e="+(E+e));
+        System.out.println("E-e="+(E-e));
+        System.out.println("E*e="+(E*e));
+        System.out.println("E/e="+(E/e));
+        System.out.println("E%e="+(E%e));
+
+        float F=125;
+        float f=250;
+        System.out.println("Float Datatype");
+        System.out.println("F+f="+(F+f));
+        System.out.println("F-f="+(F-f));
+        System.out.println("F*f="+(F*f));
+        System.out.println("F/f="+(F/f));
+        System.out.println("F%f="+(F%f));
+
+        char G=15;
+        char g=10;
+        System.out.println("Character Datatype");
+        System.out.println("G+g="+(G+g));
+        System.out.println("G-g="+(G-g));
+        System.out.println("G*g="+(G*g));
+        System.out.println("G/g="+(G/g));
+        System.out.println("G%g="+(G%g));
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ operators.java
@@ -0,0 +1,23 @@
+package Arithmetic;
+
+public class operators {
+    public static void main(String[] args) {
+        int A=10;
+        int B=30;
+        System.out.println("A="+A);
+        System.out.println("B="+B);
+        System.out.println(A+B);// 40
+
+        System.out.println(A-B);// -20
+        System.out.println(A*B);//300
+        System.out.println(A/B);//0
+        System.out.println(A++);//10
+        System.out.println(A--);//11
+        System.out.println(++A);//11
+        System.out.println(B++);//30
+        System.out.println(++B);//32
+        System.out.println(B--);//32
+        System.out.println(--B);//30
+    }
+
+}
